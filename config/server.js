b36a8eda26bb823afdb9b017c3aa85f651a0329a
Rasha Moumneh <rasha@fastmailteam.com>
2021-04-23 23:12:12 -0400

    initial strapi commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4f3435c17de297831bd5eba4f0b80303'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '83f42fba-7462-4e9a-8aab-553c638fa2d0'
+};
